7d4234c1c22daea3a2d8b0f2133fd7094374ddf8
zmoses <zmoses93@gmail.com>
2015-04-16 08:57:58 -0700

    scrap pyimgur, add def function

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -3,22 +3,25 @@ Downloads the top picture on /r/wallpapers and sets
 sets it to your wallpaper.
 """
 
-import praw # An API wrapper for Reddit
-import pyimgur # An API wrapper for Imgur
+import praw     # An API wrapper for Reddit
+import re       # Regular expression support
+import requests # Download files via HTTP
+import os
+import glob
+import sys
 from bs4 import BeautifulSoup # An HTML parser
+
+def downloadImage(imageUrl, localName):
+  response = requests.get(imageUrl)
+  if response.status_code == 200:
+    print('Downloading %s....' % (localName))
+    with open(localName, 'wb') as fo:
+      for chunk in response.iter_content(4096):
+        fo.write(chunk)
 
 user_agent = "WallpaperChanger 0.1"
 r = praw.Reddit(user_agent = user_agent)
-subreddit = r.get_subreddit("wallpapers")
+submissions = r.get_subreddit("wallpapers").get_top(limit = 5)
 
-for submission in subreddit.get_top(limit = 5):
-  # print "Title: ", submission.title
-  # print "URL:   ", submission.url, "\n"
-
-# Unique for each application:
-CLIENT_ID = "7df853035f99c0a"
-CLIENT_SECRET = "5c91dd04dc51fbfb0bf0c4e773f9563e6169b36c"
-
-im = pyimgur.Imgur(CLIENT_ID)
-image = im.get_image('S1jmapR')
-print image.link
+# Create a regex object to be used for re.search() and re.match()
+imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
