3b4605d4e7d298ab945a75f892dd6cc2f7c112dc
zmoses <zmoses93@gmail.com>
2015-04-16 10:12:07 -0700

    download files from an album in imgur

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -7,7 +7,7 @@ import praw     # An API wrapper for Reddit
 import re       # Regular expression support
 import requests # Download files via HTTP
 import os
-import glob
+import glob     # Used to search files already downloaded
 import sys
 from bs4 import BeautifulSoup # An HTML parser
 
@@ -25,3 +25,26 @@ submissions = r.get_subreddit("wallpapers").get_top(limit = 5)
 
 # Create a regex object to be used for re.search() and re.match()
 imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
+
+for submission in submissions:
+  if "imgur.com/" not in submission.url:
+    continue
+  if len(glob.glob('reddit_%s_*' % (submission.id))) > 0:
+    continue
+
+  if 'http://imgur.com/a/' in submission.url:
+    albumId = submission.url[len('http://imgur.com/a/'):]
+    htmlSource = requests.get(submission.url).text
+
+    soup = BeautifulSoup(htmlSource)
+    matches = soup.select('.album-view-image-link a')
+
+    for match in matches:
+      imageUrl = match['href']
+      if '?' in imageUrl:
+        imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+      else:
+        imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+      localName = 'reddit_%s_album_%s_imgur_%s' % (submission.id, albumId, imageFile)
+      downloadImage('http:' + match['href'], localName)
+      
