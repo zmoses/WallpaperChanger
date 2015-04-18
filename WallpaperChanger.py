0982a30a1d61d9cd4573ab0f4337e678242223e3
zmoses <zmoses93@gmail.com>
2015-04-18 10:33:29 -0700

    Styling and simplifying..

--- WallpaperChanger.py
+++ WallpaperChanger.py
@@ -11,13 +11,14 @@ import glob     # Used to search files already downloaded
 import sys
 from   bs4 import BeautifulSoup # An HTML parser
 
-def downloadImage(imageUrl, localName):
-  response = requests.get(imageUrl)
-  if response.status_code == 200:
-    print('Downloading %s....' % (localName))
-    with open(localName, 'wb') as fo:
-      for chunk in response.iter_content(4096):
-        fo.write(chunk)
+def download_image(imageUrl, submission_id, album_id, image_file):
+    localName = 'reddit_%s_album_%s_imgur_%s' % (submission_id, album_id, image_file)
+    response = requests.get(imageUrl)
+    if response.status_code == 200:
+        print('Downloading %s....' % (localName))
+        with open(localName, 'wb') as fo:
+            for chunk in response.iter_content(4096):
+                fo.write(chunk)
 
 user_agent = "WallpaperChanger 0.1"
 r = praw.Reddit(user_agent = user_agent)
@@ -27,52 +28,48 @@ submissions = r.get_subreddit("wallpapers").get_top(limit = 5)
 imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
 
 for submission in submissions:
-  if "imgur.com/" not in submission.url:
-    continue
-  if len(glob.glob('reddit_%s_*' % (submission.id))) > 0:
-    continue
+    if "imgur.com/" not in submission.url:
+        continue
+    if len(glob.glob('reddit_%s_*' % (submission.id))) > 0:
+        continue
 
-  if 'http://imgur.com/a/' in submission.url:
-    # If the image is part of an album....
-    albumId = submission.url[len('http://imgur.com/a/'):]
-    htmlSource = requests.get(submission.url).text
+    if 'http://imgur.com/a/' in submission.url:
+        # If the image is part of an album....
+        albumId = submission.url[len('http://imgur.com/a/'):]
+        htmlSource = requests.get(submission.url).text
 
-    soup = BeautifulSoup(htmlSource)
-    matches = soup.select('.album-view-image-link a')
+        soup = BeautifulSoup(htmlSource)
+        matches = soup.select('.album-view-image-link a')
 
-    for match in matches:
-      imageUrl = match['href']
-      if '?' in imageUrl:
-        imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
-      else:
-        imageFile = imageUrl[imageUrl.rfind('/') + 1:]
-      localName = 'reddit_%s_album_%s_imgur_%s' % (submission.id, albumId, imageFile)
-      downloadImage('http:' + match['href'], localName)
+        for match in matches:
+            imageUrl = match['href']
+            if '?' in imageUrl:
+                imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+            else:
+                imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+            download_image('http:' + match['href'], submission.id, albumId, imageFile)
       
-  elif 'http://i.imgur.com/' in submission.url:
-    # If the image is a direct link....
-    mo = imgurUrlPattern.search(submission.url)
-    imgurFilename = mo.group(2)
-    if '?' in imgurFilename:
-      imgurFilename = imgurFilename[:imgurFilename.find('?')]
-    localName = 'reddit_%s_album_NA_imgur_%s' % (submission.id, imgurFilename)
-    downloadImage(submission.url, localName)
+    elif 'http://i.imgur.com/' in submission.url:
+        # If the image is a direct link....
+        mo = imgurUrlPattern.search(submission.url)
+        imgurFilename = mo.group(2)
+        if '?' in imgurFilename:
+            imgurFilename = imgurFilename[:imgurFilename.find('?')]
+        download_image(submission.url, submission.id, 'NA', imgurFilename)
 
-  elif 'http://imgur.com/' in submission.url:
-    # If the image is on a page on Imgur as the only image
-    htmlSource = requests.get(submission.url).text
-    print(htmlSource)
-    soup = BeautifulSoup(htmlSource)
-    imageUrl = soup.select('.image a')[0]['href']
-    
-    if imageUrl.startswith('//'):
-      imageUrl = 'http:' + imageUrl
-    imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
+    elif 'http://imgur.com/' in submission.url:
+        # If the image is on a page on Imgur as the only image
+        htmlSource = requests.get(submission.url).text
+        print(htmlSource)
+        soup = BeautifulSoup(htmlSource)
+        imageUrl = soup.select('.image a')[0]['href']
+        
+        if imageUrl.startswith('//'):
+            imageUrl = 'http:' + imageUrl
+        imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
 
-    if '?' in imageUrl:
-      imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
-    else:
-      imageFile = imageUrl[imageUrl.rfind('/') + 1:]
-
-    localName = 'reddit_%s_album_NA_imgur_%s' % (submission.id, imageFile)
-    downloadImage(imageUrl, localName)
+        if '?' in imageUrl:
+            imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+        else:
+            imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+        download_image(imageUrl, submission.id, 'NA', imageFile)
